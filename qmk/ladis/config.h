c596ec0d6072068a8ceb2a62aeace0a6f2684064
jsmilie <31410428+jsmilie@users.noreply.github.com>
2022-10-15 14:59:27 -0500

    Add files via upload

--- qmk/ladis/config.h
+++ qmk/ladis/config.h
@@ -7,9 +7,8 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    Ladis
+#define MANUFACTURER    Pelican
 #define PRODUCT         LadisPad
-#define DESCRIPTION     macropad
 
 /* key matrix size */
 #define MATRIX_ROWS 1
@@ -17,7 +16,6 @@
 
 /* key matrix pins */
 #define DIRECT_PINS { { D3, B1, D4, C6, D7, E6, B4, B5, F4, F7, B3 } }
-#define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
@@ -29,7 +27,7 @@
 #endif
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 5
+#define DEBOUNCE 5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
@@ -42,12 +40,8 @@
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
 )
 
-/* prevent stuck modifiers */
-#define PREVENT_STUCK_MODIFIERS
-
-
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
 #ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 0
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
@@ -56,5 +50,13 @@
 
 #endif
 
-#define ENCODERS_PAD_A { F5, B6 }
-#define ENCODERS_PAD_B { F6, B2 }
+#define ENCODERS_PAD_A { F6, B2 }
+#define ENCODERS_PAD_B { F5, B6 }
+
+
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x2E, 0xBF, 0x6C, 0xB6, 0xCA, 0x8C, 0xCA, 0x55}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
